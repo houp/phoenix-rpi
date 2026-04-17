ddafbe38bab1841ce623eb8c0db46979212dfc03
Witold Bołt <witold.bolt@jit.team>
2026-04-17 17:37:58 +0200

    simple sd card writing script

--- scripts/write-sdimg.sh
+++ scripts/write-sdimg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "WARNING: this only works on macOS and assumes that and SD card is in /dev/disk4"
+echo "WARNING: if your SD card is not in /dev/disk4 - you might corrupt your system (i.e. get some important data erased)"
+
 /Users/witoldbolt/phoenix-rpi/scripts/verify-rpi4b-sdimg.sh
 
 diskutil unmountDisk /dev/disk4
